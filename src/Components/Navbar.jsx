af12d38d1f32ff69d92d3259ec8081963c252a80
arunbanga <arunbanga810@gmail.com>
2022-03-22 20:04:51 +0530

    Left side content added

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,31 +1,39 @@
 import React from "react";
 import { Container } from "./Styles/Container.styles";
+import SearchIcon from '@mui/icons-material/Search';
+import {deviceMax} from '../Utilis/Device'
 import styled from "styled-components";
 const Navbar = () => {
   return (
     <>
-      <Container width="100%" height='200px' background='black'>
+      <Container width="100%" background='black'>
         <Container
           width="100%"
           display="flex"
           flexDrection="row"
           justifyContent="center"
           background='#fff'
+          box-shadow='10px 10px 0px 0px gray'
+         
         >
-       
+          
+          <Nav>
+          <Leftcontainer>
           <h2
             style={{
-              paddingLeft: "100px",
-              fontFamily: "'Raleway', sans-serif"
-            }}
+              paddingLeft:'20px'
+             }}
           >
             
             Brand
           </h2>
-          <Nav>
-          
-            <h3 style={{ padding: "10px", color: "black" }}>
-              <a style={{ color: "black", textDecoration: "none" }} href="/">
+         
+       </Leftcontainer>
+        
+         <input className='input'type='text' style={{height:'30px',width:'100%',marginTop:'20px',  }}  placeholder='Search for Products,Brand and More' />
+         <SearchIcon className='serachbar'style={{marginTop:'25px', }}/>
+            <h3 style={{ padding: "10px"}}>
+              <a style={{ color: "black", textDecoration: "none",}} href="/">
                 Home
               </a>
             </h3>
@@ -53,12 +61,38 @@ const Navbar = () => {
 };
 
 const Nav = styled.div`
-  width: 1100px;
+  width: 100%;
   font-family: "Raleway", sans-serif;
   margin: 0px;
+  box-shadow:2px 2px 5px 2px gray;
   display: flex;
-   flex-direction: row;
-  justify-content: flex-end;
-  
+  flex-direction: row;
+  h3:active{
+   border-bottom: 2px solid red
+  }
+  @media ${deviceMax.mobileL}{
+    width:100%;
+    .input{
+      
+    }
+    .serachbar{
+      
+  }
 `;
+
+const Leftcontainer= styled.div`
+width: 100%;
+display:flex;
+justify-content: flex-start;
+
+@media ${deviceMax.mobileL}{
+    width:100%;
+    display:flex;
+    justify-content:flex-start;
+    padding-right:10px;
+    margin:0px;
+  }
+`
+
+
 export default Navbar;
--- src/Components/Products.jsx
+++ src/Components/Products.jsx
@@ -1,11 +1,53 @@
 import React from 'react'
-
+import {Container} from '../Components/Styles/Container.styles'
+import styled from 'styled-components'
 const Products =()=>{
     return(
         <>
+        <Container width='100%' >
+        <Container >
+        <Sidebar>
+        
+            <h1 style={{marginLeft:'10px',fontSize:'20px',textAlign:'center'}}>Products family</h1>
+            <hr/>
+            <div style={{marginLeft:'20px'}}>
+            <h3>Shoes</h3>
+            <h3>Shirts</h3>
+            <h3>Watches</h3>
+            <h3>Belts</h3>
+            <h3>Socks</h3>
+            <h3>Mobiles</h3>
+            </div>
+          <h1 style={{marginLeft:'10px',fontSize:'20px',textAlign:'center'}}>Sizes</h1>
+          <hr/>
+          <div style={{marginLeft:'20px'}}>
+              <h3>S</h3>
+              <h3>M</h3>
+              <h3>L</h3>
+          </div>
+          
 
-        <h1>Products Page</h1>
+
+        </Sidebar>
+
+        
+
+
+
+        </Container>
+
+        </Container>
+        
         </>
     )
 }
+
+const Sidebar= styled.div`
+width:300px;
+height:100%;
+font-family: 'Raleway', sans-serif;
+background:#fff
+
+
+`
 export default Products
